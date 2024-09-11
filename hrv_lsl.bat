f5d06d552f5fee48b02017f7a303c3109fa0f2b2
Alberto Barradas <abcsds@gmail.com>
2024-09-11 14:04:48 -0600

    Add NI bands
    on 05_multiple for LaSalle experiments

---
+++ hrv_lsl.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Neurociencias\Documents\Barradas Alberto\hrv
+python "C:\Users\Neurociencias\Documents\Barradas Alberto\hrv\src\05_multiple_lsl.py"
--- src/05_multiple_lsl.py
+++ src/05_multiple_lsl.py
@@ -7,82 +7,10 @@ from bleak import BleakScanner, BleakClient
 from pylsl import StreamInfo, StreamOutlet
 from helperfuncs import interpret
 
-HR_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
-
-
-
-# Define the addresses, names, and UUIDs of devices
-devices = [
-    # {"address": "CD:4B:39:D5:62:36", "name": "HR-70ECAB5D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-    # {"address": "CB:1E:40:C8:F6:03", "name": "HR-70EC985D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-    # {"address": "DB:D1:1C:A1:57:3D", "name": "HR-70EC845D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-    # {"address": "FF:D2:0F:F3:FE:EC", "name": "HR-70ECE75D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-    # {"address": "E8:9B:59:E2:8C:71", "name": "HR-70ECD35D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-    # {"address": "F2:48:B2:FF:3E:CE", "name": "HR-70ECD05D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},  
-]
-
-# async def connect_device(device, lock):
-#     address = device["address"]
-#     client = BleakClient(address)
-#     device_name = device["name"]
-
-#     try:
-#         async with lock:
-#             await client.connect()
-#             print(f"Device Address: {client.address}")
-#             hr_data = await client.start_notify(device["uuid"], lambda data: callback(data, device_name, outlet_hr, outlet_rr))
-#             while True:
-#                 await asyncio.sleep(0.1)
-#     except KeyboardInterrupt:
-#         print(f"Stopped by user")
-#         await client.stop_notify(device["uuid"])
-#     except Exception as e:
-#         print(f"Exception: {e}")
-#     finally:
-#         await client.disconnect()
-
-# def callback(data, device_name, outlet_hr, outlet_rr):
-#     if data:
-#         data = interpret(data, device_name)
-#         print(f"{device_name} - HR: {data['hr']}")
-#         outlet_hr.push_sample([data['hr']])
-#         if "rr" in data.keys():
-#             for data_rr in data["rr"]:
-#                 print(f"{device_name} - RR: {data_rr}")
-#                 outlet_rr.push_sample([data_rr])
-#         return data
-#     else:
-#         return None
-
-# async def main_multiple_devices(devices):
-#     # Create a lock for device connection
-#     connection_lock = asyncio.Lock()
-
-#     # Create LSL outlets for all devices
-#     for device in devices:
-#         device_name = device["name"]
-#         info_hr = StreamInfo(name=f'HR_{device_name}',
-#             type='Markers',
-#             channel_count=1,
-#             channel_format='int32',
-#             source_id=f'HR_{device_name}_markers'
-#         )
-#         info_rr = StreamInfo(name=f'RR_{device_name}',
-#             type='Markers',
-#             channel_count=1,
-#             channel_format='int32',
-#             source_id=f'RR_{device_name}_markers'
-#         )
-#         outlet_hr = StreamOutlet(info_hr)
-#         outlet_rr = StreamOutlet(info_rr)
-
-#         await connect_device(device, connection_lock)
-
 async def connect_to_device(
     lock: asyncio.Lock,
     device: dict,
     address: str,
-    outlet_hr: StreamOutlet,
     outlet_rr: StreamOutlet,
 ):
     logging.info("starting %s task", address)
@@ -125,7 +53,6 @@ async def connect_to_device(
                 if data:
                     data = interpret(data)
                     logging.info(f"{device['name']} HR: {data['hr']}")
-                    outlet_hr.push_sample([data['hr']])
                     if "rr" in data.keys():
                         for data_rr in data["rr"]:
                             logging.info(f"                   RR: {data_rr}")
@@ -134,7 +61,7 @@ async def connect_to_device(
                 else:
                     return None
 
-            hr_data = await client.start_notify(HR_UUID, callback)
+            hr_data = await client.start_notify(device["uuid"], callback)
             logging.info("HR data received from %s", address)
             while True:
                 await asyncio.sleep(0.01)
@@ -165,30 +92,23 @@ async def main(devices: dict, addresses: Iterable[str]):
             source_id=f'RR_{device_name}_markers')
         for device_name in device_names
     ]
-    outlets_hr = [StreamOutlet(info_hr) for info_hr in infos_hr]
     outlets_rr = [StreamOutlet(info_rr) for info_rr in infos_rr]
     lock = asyncio.Lock()
     await asyncio.gather(
         *(
-            connect_to_device(lock, device, address, outlet_hr, outlet_rr)
-            for device, address, outlet_hr, outlet_rr in zip(devices, addresses, outlets_hr, outlets_rr)
+            connect_to_device(lock, device, address, outlet_rr)
+            for device, address, outlet_rr in zip(devices, addresses, outlets_rr)
         )
     )
 
 
 if __name__ == "__main__":
     devices = [
-        # {"address": "DB:6E:5B:87:4E:50", "name": "Polar H10", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        {"address": "C8:28:E6:77:9D:0D", "name": "Polar H10 D6B5C724", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        {"address": "A0:9E:1A:C5:36:32", "name": "Polar Sense C5363229", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        {"address": "D6:E7:A7:D1:29:AE", "name": "Polar H10 CA549123", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "DE:C5:AC:14:25:28", "name": "HR6 0039090", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "CD:4B:39:D5:62:36", "name": "HR-70ECAB5D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "CB:1E:40:C8:F6:03", "name": "HR-70EC985D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "DB:D1:1C:A1:57:3D", "name": "HR-70EC845D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "FF:D2:0F:F3:FE:EC", "name": "HR-70ECE75D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "E8:9B:59:E2:8C:71", "name": "HR-70ECD35D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
-        # {"address": "F2:48:B2:FF:3E:CE", "name": "HR-70ECD05D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
+        # {"address": "C8:28:E6:77:9D:0D", "name": "Polar H10 D6B5C724", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
+        # {"address": "D6:E7:A7:D1:29:AE", "name": "Polar H10 CA549123", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"},
+        # LaSalle
+        {"address": "DB:10:0A:6B:80:E8", "name": "Polar H10 D66F812D", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"}, # 1
+        {"address": "D8:40:B0:01:39:EF", "name": "Polar H10 D66F7F28", "uuid": "00002a37-0000-1000-8000-00805f9b34fb"}, # 2
     ]
     addresses = [d["address"] for d in devices]
     debug = False
